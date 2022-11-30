e87167d03834c45bf4bfbcc03aeeb3946d1f78e2
Admin <Admin@DESKTOP-OJP112L>
2022-12-01 00:29:06 +0200

    Add project files.

---
+++ SoftUniProjectDefending/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SoftUniProjectDefending.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
