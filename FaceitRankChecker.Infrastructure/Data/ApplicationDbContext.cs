6d5935c80cafd125d470b7291382a882522d9d0d
YuliyanIT <ulkomarulko@gmail.com>
2022-12-11 16:35:07 +0200

    Fixing bugs

--- FaceitRankChecker.Infrastructure/Data/ApplicationDbContext.cs
+++ FaceitRankChecker.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FaceitRankChecker.Infrastructure.Data
--- SoftUniProjectDefending/Program.cs
+++ SoftUniProjectDefending/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
